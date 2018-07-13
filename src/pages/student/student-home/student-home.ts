61576a35c503b16a639555e7ca26413daa88ad4c
Leonardo <leonardo.capoccia@hotmail.it>
2018-07-13 16:15:38 +0200

    Fix error when lauched on browsers

--- src/pages/student/student-home/student-home.ts
+++ src/pages/student/student-home/student-home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController, Platform } from 'ionic-angular';
 //import { SubjectListPage } from '../../common/subject-list/subject-list';
 import { FcmProvider } from '../../../providers/fcm/fcm';
 import { tap } from 'rxjs/operators';
@@ -27,30 +27,33 @@ export class StudentHomePage {
     private fcmProvider: FcmProvider,
     private toastController: ToastController,
     private localStorage: LocalStorage,
-    private subjectProvider: SubjectProvider
+    private subjectProvider: SubjectProvider,
+    private platform: Platform
   ) {
     //this.navCtrl.push(SubjectListPage)
   }
 
   ionViewDidLoad() {
-    this.localStorage.getItem('loggedUser').subscribe((user: User) => {
+    if (this.platform.is('ios') || this.platform.is('android')) {
+      this.localStorage.getItem('loggedUser').subscribe((user: User) => {
 
-          this.fcmProvider.getToken(user).then(() => {
-            // Subscribe to single notifications
-            this.fcmProvider.listenToNotifications().pipe(
-              tap(msg => this.createToastMessage(msg))
-            ).subscribe();
+            this.fcmProvider.getToken(user).then(() => {
+              // Subscribe to single notifications
+              this.fcmProvider.listenToNotifications().pipe(
+                tap(msg => this.createToastMessage(msg))
+              ).subscribe();
 
-            // Subscribe to all topics for each course of study
-            this.subjectProvider.getAllByCourseOfStudy(user.courseOfStudy).subscribe(list => {
-              list.forEach(cs => {
-                this.fcmProvider.subscribeToTopic(cs.name.replace(' ', '')).pipe(
-                  tap(msg => this.createToastMessage(msg))
-                ).subscribe();
+              // Subscribe to all topics for each course of study
+              this.subjectProvider.getAllByCourseOfStudy(user.courseOfStudy).subscribe(list => {
+                list.forEach(cs => {
+                  this.fcmProvider.subscribeToTopic(cs.name.replace(' ', '')).pipe(
+                    tap(msg => this.createToastMessage(msg))
+                  ).subscribe();
+                })
               })
-            })
-          });
-    });
+            });
+      });
+    }
   }
 
   createToastMessage(msg) {

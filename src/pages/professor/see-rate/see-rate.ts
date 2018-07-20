61a3c6f5ed5798c4ae43862213ff1c66f1e07018
Leonardo <leonardo.capoccia@hotmail.it>
2018-07-20 18:25:08 +0200

    Code cleaning

--- src/pages/professor/see-rate/see-rate.ts
+++ src/pages/professor/see-rate/see-rate.ts
@@ -32,40 +32,34 @@ export class SeeRatePage {
   average : number;
 
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,
-    private evaluationProvider : EvaluationProvider) 
-    {
-
-
-    if ( navParams.data.type === 'lesson') {
-      console.log('era una lezione');
-      
+  constructor (
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private evaluationProvider : EvaluationProvider
+  ) {
+    if (navParams.data.type === 'lesson') {
       this.lesson = navParams.data.body;
 
       this.subjectName = this.lesson.subject.name;
       this.secondField = "the lesson of the "
- this.dateS = this.lesson.timeSlot.startTime;
- this.dateE = this.lesson.timeSlot.endTime;
+      this.dateS = this.lesson.timeSlot.startTime;
+      this.dateE = this.lesson.timeSlot.endTime;
 
       this.evaluationProvider.getEvaluationByLesson(this.lesson).subscribe(list => {
         this.evaluations = list;
         this.average = 0;
         var i;
-        for (i = 0; i < this.evaluations.length; i++) { 
-          this.average = this.average + this.evaluations[i].score ;
+        for (i = 0; i < this.evaluations.length; i++) {
+          this.average = this.average + this.evaluations[i].score;
         }
-        this.average = this.average /  this.evaluations.length;
-
+        this.average = this.average / this.evaluations.length;
       });
-
-        
     }
-    
-    if ( navParams.data.type ===  'document') {
-      console.log('era una document');
+
+    if (navParams.data.type === 'document') {
       this.document = navParams.data.body;
       this.subjectName = this.document.lesson.subject.name;
-      this.secondField = 'the document "' + this.document.name +  '" of the '
+      this.secondField = 'the document "' + this.document.name + '" of the '
       this.dateS = this.document.lesson.timeSlot.startTime;
       this.dateE = this.document.lesson.timeSlot.endTime;
 
@@ -73,23 +67,12 @@ export class SeeRatePage {
         this.evaluations = list;
         this.average = 0;
         var i;
-        for (i = 0; i < this.evaluations.length; i++) { 
-          this.average =  this.average + this.evaluations[i].score ;
+        for (i = 0; i < this.evaluations.length; i++) {
+          this.average = this.average + this.evaluations[i].score;
         }
-          this.average = this.average /  this.evaluations.length;
-        
-      
-
+        this.average = this.average / this.evaluations.length;
       });
-      
-       
     }
-
-
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SeeRatePage');
-  }
+    }
 
 }

076044a64144c8bb4728edd96a98ef08daf03bb2
Leonardo <leonardo.capoccia@hotmail.it>
2018-07-24 17:59:51 +0200

    Clean code

--- src/pages/student/rate/rate.ts
+++ src/pages/student/rate/rate.ts
@@ -15,7 +15,7 @@ import { LocalStorage } from '../../../../node_modules/@ngx-pwa/local-storage';
 
 @IonicPage()
 @Component({
-  selector: 'page-rate', 
+  selector: 'page-rate',
   templateUrl: 'rate.html',
 })
 export class RatePage {
@@ -29,63 +29,50 @@ export class RatePage {
   dateS : number;
   dateE: number;
 
-  constructor(public navCtrl: NavController, 
+  constructor(
+    public navCtrl: NavController,
     private toastController: ToastController,
     public navParams: NavParams,
-     private evaluationProvider : EvaluationProvider,
-     private localStorage: LocalStorage
-  
-    ) {
+    private evaluationProvider: EvaluationProvider,
+    private localStorage: LocalStorage
+  ) {
 
-   // this.evaluation.sender = ;
-   this.localStorage.getItem('loggedUser').subscribe((user: User) => {
-    this.loggedUser = user; 
+    this.localStorage.getItem('loggedUser').subscribe((user: User) => {
+      this.loggedUser = user;
 
-  });
-if ( navParams.data.type === 'lesson') {
-  console.log('era una lezione');
-  
-  this.lesson = navParams.data.body;
-  this.evaluation.recipientType = 'LESSON'
-  this.evaluation.recipientL = this.lesson;
-  this.subjectName = this.lesson.subject.name;
-  this.secondField = "the lesson of the "
- this.dateS = this.lesson.timeSlot.startTime;
- this.dateE = this.lesson.timeSlot.endTime;
-    
-}
+    });
 
-if ( navParams.data.type ===  'document') {
-  console.log('era una document');
-  this.document = navParams.data.body;
-  this.evaluation.recipientType = 'DOCUMENT';
-  this.evaluation.recipientD = this.document;
-  this.subjectName = this.document.lesson.subject.name;
-  this.secondField = 'the document "' + this.document.name +  '" of the '
-  this.dateS = this.document.lesson.timeSlot.startTime;
-  this.dateE = this.document.lesson.timeSlot.endTime;
+    if (navParams.data.type === 'lesson') {
+      this.lesson = navParams.data.body;
+      this.evaluation.recipientType = 'LESSON'
+      this.evaluation.recipientL = this.lesson;
+      this.subjectName = this.lesson.subject.name;
+      this.secondField = "the lesson of the "
+      this.dateS = this.lesson.timeSlot.startTime;
+      this.dateE = this.lesson.timeSlot.endTime;
+    }
 
-  
-   
-}
-  }
-
-  ionViewDidLoad() {
-
- 
-
-
-    console.log('ionViewDidLoad RatePage');
+    if (navParams.data.type === 'document') {
+      this.document = navParams.data.body;
+      this.evaluation.recipientType = 'DOCUMENT';
+      this.evaluation.recipientD = this.document;
+      this.subjectName = this.document.lesson.subject.name;
+      this.secondField = 'the document "' + this.document.name + '" of the '
+      this.dateS = this.document.lesson.timeSlot.startTime;
+      this.dateE = this.document.lesson.timeSlot.endTime;
+    }
   }
 
   saveEvaluation() {
     this.evaluation.sender = this.loggedUser;
+    if (this.evaluation.score < 0 || this.evaluation.score > 5) {
+      this.createToastMessage("The score must be between 0 and 5.");
+      return;
+    }
     this.evaluationProvider.saveEvaluation(this.evaluation).subscribe(evaluation => {
-      
-      
       this.createToastMessage("Thanks for your evaluation");
       this.navCtrl.pop();
-     
+
     }, error => {
       this.createToastMessage("Something went wrong, try again later");
     });
@@ -98,7 +85,4 @@ if ( navParams.data.type ===  'document') {
     });
     toast.present();
   }
-
-
-
 }
